fb1a999cbb95c12da141b57972f6b0b2947591c2
suti <sutiraj.21@gmail.com>
2025-02-22 16:33:05 +0530

    ok

--- lib/View/home_screen.dart
+++ lib/View/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:inidiahiringg/View/settings.dart';
+import 'package:inidiahiringg/View/settings1.dart';
 import 'Job_Detail_Screen.dart';
 import 'Subscription.dart';
 import 'View_All.dart';
@@ -323,7 +325,9 @@ class _HomeScreenState extends State<HomeScreen> {
               top: 0,
               right: 0,
               child: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => AccountSettingsScreen()));
+                },
                 icon: const Icon(CupertinoIcons.heart, color: Colors.red),
               ),),
           ],),
